6f4318e4cd935f99a7122e0f7df439bc2e14c82d
Pharina <ic21b013@technikum-wien.at>
2023-01-29 23:04:25 +0100

    Implemented the Code

--- src/main/java/at/fhtw/bic/markingapiv2/Controller/MarkingAPIv2Controller.java
+++ src/main/java/at/fhtw/bic/markingapiv2/Controller/MarkingAPIv2Controller.java
@@ -1,10 +1,29 @@
 package at.fhtw.bic.markingapiv2.Controller;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
 public class MarkingAPIv2Controller {
-
+    @GetMapping("api/marking")
+    public int calculateGrade(@RequestParam int percentage) {
+        if (0 <= percentage && percentage < 50) {
+            return 5;
+        }
+        else if (percentage < 63) {
+            return 4;
+        }
+        else if (percentage < 75) {
+            return 3;
+        }
+        else if (percentage < 88) {
+            return 2;
+        }
+        else if (percentage <= 100) {
+            return 1;
+        }
+        return -1; // wrong input
+    }
 }
