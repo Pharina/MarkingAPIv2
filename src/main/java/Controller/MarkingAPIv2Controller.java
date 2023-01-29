d02ee89cb9290d69bf89d55cabcb0458bdec2fee
Pharina <ic21b013@technikum-wien.at>
2023-01-29 22:26:55 +0100

    Added to the right package / Added Restcontroller

--- src/main/java/Controller/MarkingAPIv2Controller.java
+++
@@ -1,4 +0,0 @@
-package Controller;
-
-public class MarkingAPIv2Controller {
-}
--- src/main/java/at/fhtw/bic/markingapiv2/MarkingApIv2Application.java
+++ src/main/java/at/fhtw/bic/markingapiv2/MarkingApIv2Application.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MarkingApIv2Application {
 
     public static void main(String[] args) {
+
         SpringApplication.run(MarkingApIv2Application.class, args);
+
     }
 
 }
